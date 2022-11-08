d981769983dc71102f4968dfbc3242539ffc74a9
AleksandrVavilov <vavilov3388@gmail.com>
2022-11-08 13:56:59 +0300

    Initial commit

---
+++ src/test/java/com/company/schoolhogwarts/SchoolHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.schoolhogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
